1b5d97f81e78540bf5200cec513dd428cf4a5169
Kostya Krutenuk <capralkoooooo@gmail.com>
2017-02-05 23:07:57 +0200

    Zakharova 1

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int s = -1;
+            while (s != 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Выберите задание, рузультат которого вы хотите увидеть");
+                Console.WriteLine("1 - Максимальное число");
+                Console.WriteLine("2 - Кошка или собака");
+                Console.WriteLine("3 - Пора года");
+                Console.WriteLine("4 - Плохо или хорошо");
+                Console.WriteLine("5 - День недели");
+                Console.WriteLine("6 - Программа для такси");
+                Console.WriteLine("7 - Проверка числа на простоту");
+                Console.WriteLine("8 - Лоторея");
+                Console.WriteLine("9 - Гривны, евро, доллары");
+                Console.WriteLine("0 - Выход");
+                s = Convert.ToInt32(Console.ReadLine());
+                switch (s)
+                {
+                    case 1:
+                        double a, b;
+                        Console.WriteLine("Число 1  = ");
+                        a = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Число 2  = ");
+                        b = Convert.ToDouble(Console.ReadLine());
+                        if (a < b)
+                        {
+                            Console.Write(" Максимальное число  = " + b);
+                        }
+                        else
+                        {
+                            Console.Write(" Максимальное число = " + a);
+                        }
+                        break;
+                    case 2:
+                        string c;
+                        Console.Write("введите \"мяу\" или \"гав\" = ");
+                        c = Console.ReadLine();
+                        if (c == "мяу") Console.WriteLine("Покорми кота :)");
+                        if (c == "гав") Console.WriteLine("Погуляй с собакой :ъ");
+                        break;
+                    case 3:
+                        Console.Write("Введите порядковый номер месяца: ");
+                        int d;
+                        d = Convert.ToInt32(Console.ReadLine());
+                        if (d == 1 || d == 2 || d == 12) Console.WriteLine("Зима");
+                        else
+                        {
+                            if (d >= 3 && d <= 5) Console.WriteLine("Весна");
+                            else
+                            {
+                                if (d >= 6 && d <= 8) Console.WriteLine("Лето");
+                                else
+                                {
+                                    if (d >= 9 && d <= 11) Console.WriteLine("Осень");
+                                    else
+                                    {
+                                        Console.WriteLine("На этой планете такого месяца нет :ъ");
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("\"0\" или \"1\"");
+                        int i;
+                        i = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(i == 0 ? "Плохо" : "Хорошо");
+                        break;
+                    case 5:
+                        Console.WriteLine("Введите порядковый номер дня недели(от 1 до 7)");
+                        int day;
+                        day = Convert.ToInt32(Console.ReadLine());
+                        switch (day)
+                        {
+                            case 1: Console.WriteLine("Понедельник"); break;
+                            case 2: Console.WriteLine("Вторник"); break;
+                            case 3: Console.WriteLine("Среда"); break;
+                            case 4: Console.WriteLine("Четверг"); break;
+                            case 5: Console.WriteLine("Пятница"); break;
+                            case 6: Console.WriteLine("Суббота"); break;
+                            case 7: Console.WriteLine("Воскресенье"); break; 
+                        }
+                        break;
+                    case 6:
+                        int km, min, summa = 0;
+                        Console.Write("Километраж: ");
+                        km = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Простой: ");
+                        min = Convert.ToInt32(Console.ReadLine());
+                        if (km >= 1 && km <= 5) summa = 20;
+                        else
+                        {
+                            summa = ((km - 5) * 3) + 20;
+                        }
+                        Console.WriteLine("Summa proezda = " + (summa + min));
+                        break;
+                    case 7:
+                        int number;
+                        bool Prime = true;
+                        Console.Write("Введите число: ");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        double stop = Math.Sqrt(number);
+                        if (number % 2 == 0)
+                        {
+                            Prime = false;
+                        }
+                        else
+                        {
+                            for (int counter = 3; counter <= stop; counter += 2)
+                            {
+                                if (number % counter == 0)
+                                {
+                                    Prime = false;
+                                    break;
+                                }
+                            }
+                        }
+                        string str;
+                        Console.WriteLine(str = (Prime)? "Простое": "Не простое");
+                        break;
+                    case 8:
+                        double deck;
+                        int R = 0;
+                        Random rand = new Random();
+                        Console.Write("Ваша ставка: ");
+                        deck = Convert.ToDouble(Console.ReadLine());
+                        R = rand.Next(1, 13);
+                        Console.WriteLine("" + R);
+                        if (R >= 1 && R <= 5) Console.WriteLine("Вы проиграли:(");
+                        if (R >= 6 && R <= 8) Console.WriteLine("Ваша ставка = " + deck);
+                        if (R >= 9 && R <= 11) Console.WriteLine("Ваша ставка  = " + (deck * 2));
+                        if (R > 11 && R < 13) Console.WriteLine("Ваша ставка" + (10 * deck));
+                        break;
+                    case 9:
+                        int vi;
+                        double S, s1, s2;
+                        Console.Write("Введите сумму: ");
+                        S = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("1 - гривны, 2 - доллар, 3 - евро");
+                        vi = Convert.ToInt32(Console.ReadLine());
+                        switch (vi)
+                        {
+                            case 1:
+                                Console.WriteLine("Доллар " + Math.Round(s1 = S * 0.037, 2));
+                                Console.WriteLine("Евро " + Math.Round(s2 = S * 0.034, 2));
+                                break;
+                            case 2:
+                                Console.WriteLine("Гривна " + Math.Round(s1 = S / 0.037, 2));
+                                Console.WriteLine("Евро " + Math.Round(s2 = S * 93, 2));
+                                break;
+                            case 3:
+                                Console.WriteLine("Гривна " + Math.Round(s1 = S / 0.034, 2));
+                                Console.WriteLine("Доллар " + Math.Round(s2 = S / 0.93, 2));
+                                break;
+                        }
+
+                        break;
+                }
+                Console.WriteLine("Для продолжения нажмите Enter");
+                Console.ReadLine();     
+            }     
+        }
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int s = -1;
+            while (s != 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Выберите задание");
+                Console.WriteLine("1 - Таблица");
+                Console.WriteLine("2 - Разряды");
+                Console.WriteLine("3 - Больше или меньше");
+                Console.WriteLine("4 - Квадраты чисел");
+                Console.WriteLine("5 - Сренее арифметическое 1");
+                Console.WriteLine("6 - Сренее арифметическое 2");
+                Console.WriteLine("7 - Комбинации");
+                Console.WriteLine("8 - Пароль");
+                Console.WriteLine("0 - Выход");
+                s = Convert.ToInt32(Console.ReadLine());
+                switch (s)
+                {
+                    case 1:
+                        int for_table;
+                        Console.Write("Введите число: ");
+                        for_table = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 1; i <= 20; i++)
+                        {
+                            Console.WriteLine("*" + i + "    " + for_table * i);
+                        }
+                        break;
+                    case 2:
+                        int N, digits=0;
+                        Console.Write("Введите число: ");
+                        N = Convert.ToInt32(Console.ReadLine());
+                        do
+                        {
+                            digits++;
+                            N /= 10;
+                        }
+                        while (N != 0);
+                        Console.WriteLine("Число разрядов: " + digits);
+                        break;
+                    case 3:
+                        Random rnd = new Random();
+                        int ch = 0, t;
+                        t = rnd.Next(1, 147);
+                        while (t != ch)
+                        {
+                            Console.Write("Введите число: ");
+                            ch = Convert.ToInt32(Console.ReadLine());
+                            if (t > ch)
+                            {
+                                Console.WriteLine("Больше");
+                                Console.ReadLine();
+                            }
+                            else
+                            {
+                                if (t < ch)
+                                {
+                                    Console.WriteLine("Меньше");
+                                    Console.ReadLine();
+                                }
+                            }
+                        }
+                        Console.WriteLine("В яблочко");
+                        break;
+                    case 4:
+                        int Max;
+                        Console.Write("Введите число: ");
+                        Max = Convert.ToInt32(Console.ReadLine());
+                        int cur = 0, for_pow = 1;
+                        while (for_pow * for_pow < Max)
+                        {
+                            cur = for_pow * for_pow;
+                            for_pow++;
+                            Console.Write(cur+"    ");
+                        }
+                        break;
+                    case 5:
+                        int[] Arr=new int [5];
+                        double sum=0;
+                        for (int i = 0; i < 5; i++)
+                        {
+                            Console.Write("элемент["+Convert.ToString(i+1)+"] = ");
+                            Arr[i] = Convert.ToInt32(Console.ReadLine());
+                            sum += Arr[i];
+                        }
+                        sum /= 5;
+                        Console.WriteLine("Среднее=" + sum);
+                        break;
+                    case 6:
+                        Random rand = new Random();
+                        int[] Mass = new int[5];
+                        double Sum = 0;
+                        for (int i = 0; i < 5; i++)
+                        {
+                            Mass[i] = rand.Next(-10000, 10000);
+                            Console.WriteLine("элемент[" + (i + 1) + "] = " + Mass[i]);
+                            Sum += Mass[i];
+                        }
+                        Sum /= 5;
+                        Console.WriteLine("Среднее = " + Sum);
+                        break;
+                    case 7:
+                        int A, B;
+                        A = 0; B = 32;
+                        while(B>=0)
+                        {
+                            Console.WriteLine("Кроликов = " + A + "    Гусей = " + B);
+                            B -= 2;
+                            A += 1;
+                        }
+                        break;
+                    case 8:
+                        string str="", p="root";
+                        while (str != p)
+                        {
+                            Console.Clear();
+                            Console.Write("Пароль: ");
+                            str = Console.ReadLine();
+                            if (str != p)
+                            {
+                                Console.WriteLine("Неверно");
+                            }
+                        }
+                        Console.Clear();
+                        Console.WriteLine("Верно");
+                        break;
+                }
+                Console.WriteLine("Для продолжения нажмите Enter");
+                Console.ReadLine();     
+            }     
+        }
+    }
+}
+
